7ddf4a5eae29ccf32d92a5dd8ea41a5c603d4faf
shar1212 <143186844+shar1212@users.noreply.github.com>
2023-08-25 15:15:44 +0300

    Add files via upload
    I made a short code converting celcius tempature to kelvin.

---
+++ Temp.py
@@ -0,0 +1,10 @@
+#18.8.23
+#Converstion from Celcius to Kelvin temp
+#-------------------------------------
+#Enter celcius temp
+cel=input('Enter celcius temp')
+
+#Combine the integers together
+
+print(int(cel)+ 273.15)
+
